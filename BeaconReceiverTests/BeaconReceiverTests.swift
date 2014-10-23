1fa9d7dcbc066547e360818f74af8bfd836434a5
kunihiko-harada <sthsoulful@gmail.com>
2014-10-23 21:22:42 +0900

    Initial Commit

---
+++ BeaconReceiverTests/BeaconReceiverTests.swift
@@ -0,0 +1,36 @@
+//
+//  BeaconReceiverTests.swift
+//  BeaconReceiverTests
+//
+//  Created by harada on 2014/10/23.
+//  Copyright (c) 2014年 harada. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class BeaconReceiverTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
